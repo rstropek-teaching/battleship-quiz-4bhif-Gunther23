9b78bdbbe786f4f36355a99ff2653452df74aea1
Gunther23 <guenther.julian@gmx.at>
2017-10-10 11:01:52 +0200

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -18,13 +18,32 @@ $(() => {
     tr.appendTo(battleground);
   }
 
+
+  
   $('#generate').click(() => {
+
+    var randomlength;
+    var randomwidth;
+    var direction;
+    var ships = [2,3,3,4,5];
+   
+    direction = Math.floor()*2;
+    for(let i = 0;i<ships.length;i++){
+      randomlength = Math.floor()*8;
+      randomwidth = Math.floor()*8;
+      for (let j = 0;j<ships[i].length;i++){
+        $('td[data-r="randomlength"][data-r="randomwidth"]').removeClass('water').addClass('ship');
+      }
+      
+    }
+    
+
     // Here you have to add your code for building a random battleground.
 
     // Tip: The next line of code demonstrates how you can select a table cell
     // using coordinates, remove CSS classes and add CSS classes. 
     $('td[data-r="1"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="2"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="3"][data-c="1"]').removeClass('water').addClass('ship');
+  //  $('td[data-r="2"][data-c="1"]').removeClass('water').addClass('ship');
+  //  $('td[data-r="3"][data-c="1"]').removeClass('water').addClass('ship');
   });
 });
